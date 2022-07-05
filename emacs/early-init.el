cd4ca58493795c01f4161d369e77b1b28df9fa59
Áquila Freitas <aquilacf@protonmail.com>
2022-07-05 13:07:00 +0100

    Add latest emacs config. Work in progress.

---
+++ emacs/early-init.el
@@ -0,0 +1,225 @@
+;;;;;;;;;;;;;;;;;;
+;; GC Threshold ;;
+;;;;;;;;;;;;;;;;;;
+
+(defconst GC_CONST_THREASHOLD_ORIGINAL gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+
+; Empty file names list
+(defconst FILE_NAME_HANDLER_ALIST_ORIGINAL file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(add-hook 'after-init-hook
+		`(lambda ()
+		   (setq gc-cons-threshold GC_CONST_THREASHOLD_ORIGINAL)
+		   (setq file-name-handler-alist FILE_NAME_HANDLER_ALIST_ORIGINAL)
+		   ) t)
+
+(run-with-idle-timer 5 t #'garbage-collect)
+
+
+;; Disable package
+(setq package-enable-at-startup nil)
+
+;; Define system constants
+(defconst IS_MAC       (eq system-type 'darwin))
+(defconst IS_LINUX     (eq system-type 'gnu/linux))
+(defconst IS_WINDOWS   (memq system-type '(cygwin windows-nt ms-dos)))
+
+;Clean up unnecessary option list.
+(unless IS_MAC   (setq command-line-ns-option-alist nil))
+(unless IS_LINUX (setq command-line-x-option-alist nil))
+
+
+;; Define user constants
+(defconst DIR_ROOT (expand-file-name user-emacs-directory))
+(defconst DIR_CACHE (concat DIR_ROOT "cache/"))
+(defconst DIR_SNIPPETS (concat DIR_ROOT "snippets/"))
+(defconst DIR_PACKAGES (concat DIR_ROOT "packages/"))
+(mkdir DIR_CACHE t)
+;(mkdir DIR_SNIPPETS)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;;;;;;;;;;;;;;;;;;;
+;; Handling Files ;;
+;;;;;;;;;;;;;;;;;;;;
+
+;; Custom files
+(setq custom-file (concat DIR_CACHE "custom.el"))
+(load custom-file 'noerror 'nomessage)
+
+(global-auto-revert-mode t)
+(setq-default global-auto-revert-non-file-buffers t)
+(setq create-lockfiles nil)
+
+;; Charset
+(set-charset-priority 'unicode)
+(prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
+(setq require-final-newline t)
+
+;; Recentf
+(setq-default recentf-max-menu-items 25)
+(setq-default recentf-max-saved-items 25)
+(setq-default recentf-save-file (concat DIR_CACHE "recentf"))
+(recentf-mode t)
+
+;; Save Place
+(setq-default save-place-file (concat DIR_CACHE "places"))
+(save-place-mode t)
+
+;; History
+(setq history-length 30)
+(setq-default savehist-file (concat DIR_CACHE "history"))
+(savehist-mode t)
+(setq-default url-history-file (concat DIR_CACHE "url-history"))
+
+;; Backups
+(defconst DIR_BACKUPS (concat DIR_ROOT "backups/"))
+;(mkdir DIR_BACKUPS t)
+(setq backup-inhibited t)
+(setq make-backup-files nil)
+
+
+;; Autosaves
+(defconst DIR_AUTOSAVES (concat DIR_ROOT "autosaves/"))
+;(mkdir DIR_AUTOSAVES t)
+(setq auto-save-default nil)
+(setq auto-save-interval 0)
+(setq auto-save-timeout 0)
+(setq auto-save-list-file-prefix nil)
+
+
+;; Interface
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(modify-all-frames-parameters '((fullscreen . maximized)
+                                (internal-border-width . 1)
+                                (vertical-scroll-bars . nil)
+                                (tool-bar-lines . 0)
+                                (ns-appearance . dark)
+                                (font . "FiraCode Nerd Font-14")))
+
+
+
+(defun display-startup-echo-area-message()
+  "Message that will display on the footer when opening EMACS."
+  (message (concat "Loaded in " (emacs-init-time) ".")))
+
+(setq inhibit-startup-message t)
+(setq inhibit-default-init t)
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'fundamental-mode)
+(setq inhibit-splash-screen t)
+
+
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
+(setq use-dialog-box nil)
+
+;; Line numbers
+(setq-default display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+(global-visual-line-mode)
+
+(column-number-mode)
+(delete-selection-mode t)
+
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+;; Parenthesis
+;(set-face-attribute 'show-paren-match nil :foreground 'unspecified :background 'unspecified :weight 'extra-bold :underline t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode t)
+
+
+;; Tramp
+(setq tramp-persistency-file-name (concat DIR_CACHE "tramp"))
+(setq-default tramp-default-method "ssh")
+
+					; Projects
+(setq project-list-file (concat DIR_CACHE "projects"))
+
+;;;;;;;;;;;
+;; Other ;;
+;;;;;;;;;;;
+(setq large-file-warning-threshold nil)
+(setq vc-follow-symlinks t)
+(setq idle-update-delay 1.0)
+(setq inhibit-compacting-font-caches t)
+(setq fast-but-imprecise-scrolling t)
+(setq redisplay-skip-fontification-on-input t)
+(setq auto-mode-case-fold nil)
+(setq-default bidi-display-reordering 'left-to-right
+	      bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+(setq-default ffap-machine-p-known 'reject)
+(setq frame-inhibit-implied-resize t)
+
+
+;;;;;;;;;;;;;;
+;; Straight ;;
+;;;;;;;;;;;;;;
+(setq-default straight-base-dir DIR_CACHE)
+(unless (featurep 'straight)
+  ;; Bootstrap straight.el
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" DIR_CACHE))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el" 'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
+
+;; Use Package
+(straight-use-package 'use-package)
+(straight-use-package 'use-package-ensure-system-package)
+(setq-default straight-use-package-by-default t
+	      use-package-expand-minimally t
+	      use-package-always-defer t) 
+
+;;;;;;;;;;;
+;; Theme ;;
+;;;;;;;;;;;
+(use-package doom-themes
+  :demand t
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  :config
+  (load-theme 'doom-one t)
+  (doom-themes-org-config))
+
+(defvar current-theme-style "d")
+(defun my/toggle-theme ()
+  "Toggle theme style."
+  (interactive)
+  (if (string-equal current-theme-style "d")
+      (progn
+	(disable-theme 'doom-one) (load-theme 'doom-one-light t) (setq current-theme-style "l"))
+    (progn
+      (disable-theme 'doom-one-light) (load-theme 'doom-one t) (setq current-theme-style "d"))))
+
+
+(use-package doom-modeline
+  :demand t
+  :custom (doom-modeline-height 15)
+  :config (doom-modeline-mode t))
+
+;;; early-init.el ends here
+
---
+++ emacs/init.el
@@ -0,0 +1,420 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Retrieve PATH from zshrc ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun my/update-path-from-zshell()
+  "Update exec-path and PATH with zshrc PATHs."
+  (let ((path (shell-command-to-string "TERM=dumb . ~/.zshrc; echo -n $PATH")))
+    (setenv "PATH" path)
+    (setq exec-path (split-string-and-unquote path path-separator))))
+
+(when (display-graphic-p)
+  (my/update-path-from-zshell))
+
+;;;;;;;;;;;;;;;;;;
+;; Key Bindings ;;
+;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
+;; Mouse wheel
+(xterm-mouse-mode t)
+(global-set-key (kbd "<mouse-3>") (kbd "C-y"))
+(global-set-key (kbd "<mouse-4>") (kbd "C-u 1 M-v"))
+(global-set-key (kbd "<mouse-5>") (kbd "C-u 1 C-v"))
+
+(global-unset-key (kbd "C-t")) ; This is reserved to tmux
+
+(when IS_MAC
+  (setq mac-right-option-modifier nil))
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+;; Evil
+(use-package evil
+  :demand t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+;  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode t)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  ;; (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  ;; (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config (evil-collection-init))
+
+;; (use-package counsel
+;;   :bind (("C-M-j" . 'counsel-switch-buffer)
+;;          :map minibuffer-local-map
+;;          ("C-r" . 'counsel-minibuffer-history))
+;;   :custom
+;;   (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+;;   :config
+;;   (counsel-mode 1))
+
+(use-package which-key
+  :defer 5
+  :config
+  (which-key-mode t)
+  :custom (which-key-idle-delay 0.3))
+
+(use-package ivy
+  ;; :bind (("C-s" . swiper)
+  ;;        :map ivy-minibuffer-map
+  ;;        ("TAB" . ivy-alt-done)
+  ;;        ("C-l" . ivy-alt-done)
+  ;;        ("C-j" . ivy-next-line)
+  ;;        ("C-k" . ivy-previous-line)
+  ;;        :map ivy-switch-buffer-map
+  ;;        ("C-k" . ivy-previous-line)
+  ;;        ("C-l" . ivy-done)
+  ;;        ("C-d" . ivy-switch-buffer-kill)
+  ;;        :map ivy-reverse-i-search-map
+  ;;        ("C-k" . ivy-previous-line)
+  ;;        ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; (use-package ivy-rich
+;;   :after ivy
+;;   :init
+;;   (ivy-rich-mode 1))
+
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+ ; :custom
+ ; (counsel-describe-function-function #'helpful-callable)
+ ; (counsel-describe-variable-function #'helpful-variable)
+  :bind
+;  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-function] . helpful-callable)
+  ([remap describe-command] . helpful-command)
+;  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-key] . helpful-key))
+
+
+(use-package general
+  :disabled
+  :config
+  (general-create-definer my/leader-keys
+    :keymaps '(normal insert visual emacs)
+    ;:prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (my/leader-keys
+    "t"  '(:ignore t :which-key "toggles")))
+    ;"tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package hydra :disabled)
+
+;; (defhydra hydra-text-scale (:timeout 4)
+;;   "scale text"
+;;   ("j" text-scale-increase "in")
+;;   ("k" text-scale-decrease "out")
+;;   ("f" nil "finished" :exit t))
+
+;; (my/leader-keys
+;;  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+
+;;;;;;;;;;;;;;;;;
+;; Minor Modes ;;
+;;;;;;;;;;;;;;;;;
+
+(use-package editorconfig
+  :demand t
+  :config (editorconfig-mode t))
+(use-package smartparens
+  :defer 2
+  :config (smartparens-global-mode t))
+(use-package rainbow-mode
+;; Not working
+  :defer 5
+  :config (rainbow-mode t))
+
+
+
+
+
+;;;;;;;;;;;;;;
+;; Projects ;;
+;;;;;;;;;;;;;;
+(use-package projectile
+  :demand t
+  :custom
+  (projectile-known-projects-file (concat DIR_CACHE "projectile-bookmarks.eld"))
+  (projectile-cache-file (concat DIR_CACHE "projectile.cache"))
+  (projectile-switch-project-action #'projectile-dired)
+  (projectile-project-search-path '(("~/Projects" . 3)))
+  ;(projectile-completion-system 'ivy)
+  :config
+  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode))
+
+
+;;;;;;;;;;;;;;
+;; Treemacs ;;
+;;;;;;;;;;;;;;
+(use-package treemacs
+  :custom (treemacs-persist-file (concat DIR_CACHE "treemacs-persist"))
+  :bind ("§" . treemacs)
+  :config
+  (treemacs-filewatch-mode)
+  (treemacs-follow-mode))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :config
+  (advice-add 'projectile-switch-project-by-name :after #'(lambda (&rest args)
+							    (let* ((path (car args))
+								   (name (treemacs--filename path)))
+							      (treemacs-do-add-project-to-workspace path name)))))
+
+
+;;;;;;;;;;;;;
+;; Company ;;
+;;;;;;;;;;;;;
+(use-package company
+  :defer 3
+  :custom
+  (company-idle-delay 0.5)
+  (company-echo-delay 0)
+  (company-show-numbers t)
+  (company-selection-wrap-around t)
+  (company-minimum-prefix-length 3)
+  (company-tooltip-align-annotations t)
+  (company-dabbrev-downcase nil)
+  :config (global-company-mode))
+
+
+(use-package company-statistics
+  :after (company)
+  :custom (company-statistics-file (concat DIR_CACHE "company-statistics-cache.el"))
+  :config (company-statistics-mode))
+
+(use-package company-box :hook (company-mode . company-box-mode))
+
+(use-package company-quickhelp
+  :after (company)
+  :custom (company-quickhelp-delay 0)
+  :hook (company-mode . company-quickhelp-mode))
+
+(use-package company-quickhelp-terminal
+  :after (company-quickhelp)
+  :hook (company-quickhelp-mode . company-quickhelp-terminal-mode))
+
+
+;;;;;;;;;;;;;
+;; Flycheck ;;
+;;;;;;;;;;;;;
+;; (use-package flycheck
+;; 	:defer 2
+;; 	:config (global-flycheck-mode))
+
+;;;;;;;;;;;
+;; Corfu ;;
+;;;;;;;;;;;
+
+
+(use-package corfu
+  :disabled
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `corfu-excluded-modes'.
+  :init
+  (global-corfu-mode))
+
+;;;;;;;;;
+;; LSP ;;
+;;;;;;;;;
+(defconst DIR_LSP (concat DIR_CACHE "lsp"))
+(defconst DIR_DAP (concat DIR_CACHE "dap"))
+(use-package lsp-mode
+  :custom
+  (lsp-server-install-dir DIR_LSP)
+  (lsp-session-file (concat DIR_CACHE "lsp-session"))
+  (lsp-keymap-prefix "C-c l")
+  (lsp-auto-guess-root t)
+  :commands (lsp lsp-deferred)
+  :config (lsp-enable-which-key-integration t))
+
+;(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package dap-mode
+  :commands dap-debug
+  :config
+  ;; (general-define-key
+  ;;   :keymaps 'lsp-mode-map
+  ;;   :prefix lsp-keymap-prefix
+  ;;   "d" '(dap-hydra t :wk "debugger"))
+  )
+
+;;;;;;;;;;;;;;;;;
+;; Major Modes ;;
+;;;;;;;;;;;;;;;;;
+
+;;;;;;;;
+;; C# ;;
+;;;;;;;;
+(use-package tree-sitter)
+(use-package tree-sitter-langs)
+(use-package tree-sitter-indent)
+
+(defun my/csharp-mode-hook ()
+  (lsp-deferred)
+  (electric-pair-mode 1))
+
+(use-package csharp-mode
+  :mode "\\.cs\\'"
+  :hook (csharp-tree-sitter-mode . my/csharp-mode-hook) ; check this later
+  :init (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode))
+  :custom
+  (dap-netcore-install-dir DIR_DAP)
+  :config
+  (require 'dap-netcore)
+  (dap-netcore-setup))
+
+
+(use-package powershell)
+
+;;;;;;;;;;;;;;
+;; Markdown ;;
+;;;;;;;;;;;;;;
+(use-package markdown-mode
+  :mode "\\.md\\'"
+  :hook (markdown-mode . lsp-deferred))
+
+;;;;;;;;;;;;
+;; Docker ;;
+;;;;;;;;;;;;
+(use-package docker :bind ("C-c d" . docker))
+(use-package dockerfile-mode :mode "Dockerfile\\'")
+
+;;;;;;;;;;;;;;;
+;; Terraform ;;
+;;;;;;;;;;;;;;;
+(use-package terraform-mode
+  :mode "\\.tf\\'"
+  :hook (terraform-mode . lsp-deferred))
+
+;; (use-package company-terraform
+;;   :after (terraform-mode company)
+;;   :hook (terraform-mode . company-terraform-init))
+
+;;;;;;;;;;;;;;;;
+;; TypeScript ;;
+;;;;;;;;;;;;;;;;
+(use-package typescript-mode
+  :mode ("\\.ts[x]?\\'" . typescript-mode)
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (require 'dap-node)
+  (dap-node-seup))
+
+;;;;;;;;;;
+;; YAML ;;
+;;;;;;;;;;
+(use-package yaml-mode
+  :mode "\\.yml\\'"
+  :hook (yaml-mode . lsp-deferred)
+  :custom (lsp-yaml-schemas t))
+
+;;;;;;;;;;
+;; JSON ;;
+;;;;;;;;;;
+;(use-package json-mode
+;	 :ensure-system-package (vscode-json-languageserver . "yarn global add vscode-json-languageserver")
+;	 :custom (lsp-json-schemas t))
+
+;;;;;;;;;;;;;
+;; GraphQL ;;
+;;;;;;;;;;;;;
+(use-package graphql-mode
+  :mode "\\.graphql\\'"
+  :hook (graphql-mode . lsp-deferred))
+
+;;;;;;;;;;;;;;
+;; PlantUML ;;
+;;;;;;;;;;;;;;
+(use-package plantuml-mode
+  :config
+  (setq plantuml-executable-path "plantuml")
+  (setq plantuml-default-exec-mode 'executable)
+  :ensure-system-package (plantuml . plantuml)
+  :mode ("\\.p[lant]?uml\\'" . plantuml-mode))
+
+;;;;;;;;;
+;; Org ;;
+;;;;;;;;;
+(use-package org)
+(setq org-element-cache-persistent nil)
+(use-package org-bullets :hook (org-mode . org-bullets-mode))
+
+;;;;;;;;;;;
+;; C/C++ ;;
+;;;;;;;;;;;
+(use-package cmake-mode)
+
+
+
+
+;;;;;;;;;;;;
+;; Gutter ;;
+;;;;;;;;;;;;
+(use-package git-gutter
+  :defer 5 
+  :custom
+  (git-gutter:added-sign " +")
+  (git-gutter:deleted-sign " -")
+  (git-gutter:modified-sign " \u2502")
+  (git-gutter:separator-sign nil)
+  (git-gutter:unchanged-sign nil)
+  (git-gutter:lighter nil)
+  (git-gutter:window-width 2)
+  (git-gutter:visual-line t)
+  (git-gutter:update-interval 2)
+
+  :config
+  (set-face-attribute 'git-gutter:added nil :inherit 'default :foreground "green1" :background 'unspecified)
+  (set-face-attribute 'git-gutter:deleted nil :inherit 'default :foreground "red" :background 'unspecified)
+  (set-face-attribute 'git-gutter:modified nil :inherit 'default :foreground "yellow" :background 'unspecified)
+  (set-face-attribute 'git-gutter:unchanged nil :inherit 'default :foreground 'unspecified :background 'unspecified)
+  (global-git-gutter-mode))
+
+;; Magit
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
