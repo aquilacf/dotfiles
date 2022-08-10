dfb7c02e86fc96dbdb0d28e5dd6c96553475f95f
Áquila Freitas <aquilacf@protonmail.com>
2022-08-10 18:23:55 +0100

    Delete treemacs.

--- emacs/modules/my-ui.el
+++ emacs/modules/my-ui.el
@@ -33,25 +33,6 @@
   :if (display-graphic-p)
   :demand t)
 
-;;;;;;;;;;;;;;
-;; Treemacs ;;
-;;;;;;;;;;;;;;
-(use-package treemacs
-  :bind ("§" . treemacs)
-  :custom
-  (treemacs-persist-file (concat DIR_CACHE "treemacs-persist"))
-  (treemacs-last-error-persist-file (concat DIR_CACHE "treemacs-persist-at-last-error"))
-  :config
-  (treemacs-git-commit-diff-mode)
-  (treemacs-filewatch-mode)
-  (treemacs-project-follow-mode)
-  (treemacs-follow-mode))
-
-(use-package treemacs-projectile
-  :after (treemacs projectile))
-
-
-
 (use-package rainbow-mode
   :defer 5
   :config (rainbow-mode))
