f6fbe06e7491a0b810acac816f6391b4457b543c
Áquila Freitas <hi@aquilafreitas.com>
2025-08-25 10:29:58 +0100

    fix helix symbolic link

--- bin/install_helix.sh
+++ bin/install_helix.sh
@@ -5,4 +5,7 @@ HELIX="$(pwd)/../helix/src"
 export HELIX_DEFAULT_RUNTIME="$HELIX/runtime/"
 
 cargo install -vv --path "$HELIX/helix-term" --locked
-ln -fs "$HELIX/contrib/completion/hx.zsh" "../zsh/completions/_hx"
+
+# Autocompletions
+cd ../zsh/completions
+ln -fs "../../helix/src/contrib/completion/hx.zsh" "_hx"
